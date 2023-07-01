0db942fd7a094599c8d4a2e8850a69596001a72c
TigerBaseball <121096971+TigerBaseball@users.noreply.github.com>
2023-07-01 21:00:26 +0900

    add onOpen function

---
+++ src/on_oepn.js
@@ -0,0 +1,38 @@
+/**
+ * Creates a menu in the spreadsheet to manage events
+ * 
+ * Must have `create_events()', `display_events()`,`update_delete_events()` functions from the library in the target project
+ * 
+ * Example:
+ * ```
+ * function onOpen() {
+ *  library_name.onOpen();
+ * }
+ * function create_events() {
+ *  library_name.create_events();
+ * }
+ * function display_events() {
+ *  library_name.display_events();
+ * }
+ * function update_delete_events() {
+ *  library_name.update_delete_events();
+ * }
+ * 
+ * ```
+ * 
+*/
+function onOpen() {
+    let ui = SpreadsheetApp.getUi();
+    ui.createMenu('Manage Events')
+        .addItem('Create Event', 'create_events')
+        .addSeparator()
+        .addItem('Display Events', 'display_events')
+        .addSeparator()
+        .addItem('Update/Delete Event', 'update_delete_events')
+        .addSeparator()
+        .addToUi();
+}
+
+function test(){
+    console.log('test');
+}
